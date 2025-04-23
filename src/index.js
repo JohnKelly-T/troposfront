97f4a712dfbcae52232e354c49919d067b87802e
john-kelly <johnkellytemena@gmail.com>
2025-04-23 17:08:37 +0800

    Import DOM Controller

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './styles/reset.css';
 import './styles/global.css';
+import './styles/home.css';
+import { Domcontroller } from './modules/dom-controller';
+
+let domcontroller = new Domcontroller();
