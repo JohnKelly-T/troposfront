8bf685416ea610184848cad1eb10dc761e498747
john-kelly <johnkellytemena@gmail.com>
2025-05-02 15:12:45 +0800

    Import forecast css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/reset.css';
 import './styles/global.css';
 import './styles/home.css';
+import './styles/forecast.css';
 import { DomController } from './modules/dom-controller';
 
 let domController = new DomController();
