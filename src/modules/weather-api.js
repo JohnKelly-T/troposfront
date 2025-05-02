b3a723e693321a9f929705bcd199a0d89ac7873f
john-kelly <johnkellytemena@gmail.com>
2025-05-02 20:53:37 +0800

    Fix unsecure api url

--- src/modules/weather-api.js
+++ src/modules/weather-api.js
@@ -1,6 +1,6 @@
 // api key is hardcoded as this project is only a js project meant to showcase skills with handling api
 const API_KEY = '5ef6c309cf3f42dea2b45514252304';
-const BASE_URL = 'http://api.weatherapi.com/v1';
+const BASE_URL = 'https://api.weatherapi.com/v1';
 
 export async function getWeatherForecast(query) {
   const API_METHOD = '/forecast.json';
